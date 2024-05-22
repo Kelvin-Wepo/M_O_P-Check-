3c8d5ec71845a9968389edaa7f19bcdb8f65a49c
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 20:08:37 +0300

    ft:initial commit for user registration view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.conf import settings
+from .models import ObesityData
 
-# Create your views here.
+import pandas as pd
+from datetime import date
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import AppointmentForm, MentalDisorderForm, pcosDisorderForm, AppointmentDataForm, obesityDisorderForm
+from .models import Receipt, UserProfile, userHistory, DoctorUser, AppointmentData
+from django.contrib.auth.models import User
+from django import forms
+
+
+import pandas as pd
+import joblib
+import tensorflow as tf
+import numpy as np
+from django.utils import timezone
+import json
+
+from django.contrib import messages
+
+mental_disorder_model = joblib.load('static/models/mental_disorder_prediction.pkl')
+mental_disorder_encoder = joblib.load('static/encoders/mental_disorder_encoder.pkl')
+mental_disorder_output_encoder = joblib.load('static/encoders/mental_disorder_output_encoder.pkl')
+mental_disorder_df = pd.read_csv('static/mentalDisorder.csv')
+
+pcos_model = joblib.load('static/models/pcos_prediction.pkl')
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # for CSS Properties
+        self.fields['username'].widget.attrs.update({'class': 'col-md-10 form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'col-md-10 form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'col-md-10 form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'col-md-10 form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'col-md-10 form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'col-md-10 form-control'})
+
+        self.fields['username'].help_text = '<span class="text-muted">Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</span>'
+        self.fields['email'].help_text = '<span class="text-muted">Required. Inform a valid email address.</span>'
+        self.fields['password2'].help_text = '<span class="text-muted">Enter the same password as before, for verification.</span>'
+        self.fields['password1'].help_text = '<span class="text-muted"><ul class="small"><li class="text-muted">Your password can not be too similar to your other personal information.</li><li class="text-muted">Your password must contain at least 8 characters.</li><li class="text-muted">Your password can not be a commonly used password.</li><li class="text-muted">Your password can not be entirely numeric.</li></ul></span>' 
+
