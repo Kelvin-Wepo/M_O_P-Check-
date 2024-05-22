ecd4c26e0ac3bfab8e1875671635cb9368d4a543
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 21:31:40 +0300

    this commit creates pcos disorderform  for pcos disorder model

--- home/forms.py
+++ home/forms.py
@@ -40,3 +40,28 @@ class MentalDisorderForm(forms.ModelForm):
             if isinstance(field.widget, forms.Select):
                 field.empty_label = "Choose one"
 
+class pcosDisorderForm(forms.ModelForm):
+    class Meta:
+        model = pcosDisorder
+        fields = '__all__'
+        exclude = ['user']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control col-md-12'})
+            if isinstance(field.widget, forms.Select):
+                field.empty_label = "Choose one"
+                
+class obesityDisorderForm(forms.ModelForm):
+    class Meta:
+        model = obesityDisorder
+        fields = '__all__'
+        exclude = ['user']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control col-md-12'})
+            if isinstance(field.widget, forms.Select):
+                field.empty_label = "Choose one"
