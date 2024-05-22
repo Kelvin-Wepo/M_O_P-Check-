ea8b1e484dd577a110ae15ba80101f5b91d6c794
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 21:46:14 +0300

    ft: this importd neccesary modules for models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import pandas as pd
+from django.utils import timezone
+import json
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import Group, Permission
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
