c940a27d9ff2ca4249ea9be03fff4b0d8e5b1b37
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 21:53:27 +0300

    this commit defines user profile  model

--- home/models.py
+++ home/models.py
@@ -34,3 +34,18 @@ class Receipt(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     receipt_file = models.FileField(upload_to='receipts/')
 
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dob = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=(('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')))
+    height = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    profession = models.CharField(max_length=100, null=True, blank=True)
+
+    @property
+    def bmi(self):
+        if self.height and self.weight:
+            return round(self.weight / ((self.height / 100) ** 2), 2)
+        return None
+
+
