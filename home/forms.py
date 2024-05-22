d3811c6dc16f7bffbb7cbddcaeaf851f74b3eceb
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 21:27:40 +0300

    this commit creates appointment data form for appointment model

--- home/forms.py
+++ home/forms.py
@@ -5,3 +5,11 @@ class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = ['appointment_date']
+
+from .models import Appointment
+
+class AppointmentForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['appointment_date']
+
