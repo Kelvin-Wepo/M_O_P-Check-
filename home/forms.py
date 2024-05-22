6fd57d39f9dd17e9972d8ba45a8d95e87fdd735c
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 21:30:33 +0300

    this commit creates mentalform for mental disorder model

--- home/forms.py
+++ home/forms.py
@@ -13,3 +13,30 @@ class AppointmentForm(forms.ModelForm):
         model = Appointment
         fields = ['appointment_date']
 
+from .models import mentalDisorder, pcosDisorder
+
+class AppointmentDataForm(forms.ModelForm):
+    class Meta:
+        model = AppointmentData
+        fields = '__all__'
+        exclude = ['user', 'doctor', 'status']
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-control col-md-12'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control col-md-12'}),
+            'appointmentDate': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class MentalDisorderForm(forms.ModelForm):
+    class Meta:
+        model = mentalDisorder
+        fields = '__all__'
+        exclude = ['user']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control col-md-12'})
+            if isinstance(field.widget, forms.Select):
+                field.empty_label = "Choose one"
+
