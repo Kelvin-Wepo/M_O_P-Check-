2eb61a57126999b5d46c646adf14c7fe9c4cdf36
kelvin wepo <kelvinwepo7710@gmail.com>
2024-05-22 21:49:39 +0300

    this commit defines user history model

--- home/models.py
+++ home/models.py
@@ -10,3 +10,19 @@ from django.utils.translation import gettext as _
 mental_disorder_df = pd.read_csv('static/mentalDisorder.csv')
 
 
+from django.contrib.auth.models import AbstractUser
+
+
+class userHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    test_type = models.CharField(max_length = 120)
+    symptoms = models.CharField(max_length = 500)
+    result = models.CharField(max_length = 120)
+    date = models.DateField(default=timezone.now)
+    
+    def set_symptoms(self, symptoms_list):
+        self.symptoms = json.dumps(symptoms_list)
+
+    def get_symptoms(self):
+        return json.loads(self.symptoms)
+
